16b9261ef44592610526e7bef2c10cf732dd194f
kbrady478 <c23789389@mytudublin.ie>
2024-05-10 13:47:21 +0100

    Fade out and reload on death

--- Borders_Project/Assets/Scripts/Scene_Transition.cs
+++ Borders_Project/Assets/Scripts/Scene_Transition.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class Scene_Transition : MonoBehaviour
 {
@@ -13,8 +14,10 @@ public class Scene_Transition : MonoBehaviour
     [Range(0,1)]
     public float fade_Speed;
 
-    public bool start_Sequence = true;
-    public bool death_Sequence = false;
+    public bool start_Sequence;
+    public bool death_Sequence;
+    private Color fade_Transparancy;
+    
     
     //public float trans_Test;
 
@@ -23,13 +26,20 @@ public class Scene_Transition : MonoBehaviour
         Color reset_Colour = black_Screen.color;
         reset_Colour.a = 1;
         black_Screen.color = reset_Colour;
+        
+        start_Sequence = true;
+        death_Sequence = false;
+        fade_Transparancy = black_Screen.color;
     }
 
     private void Update()
     {
-        if (start_Sequence = true && black_Screen.color.a >= 0)
+        print("start: "+start_Sequence);
+        print("death: "+death_Sequence);
+
+        if (start_Sequence == true && black_Screen.color.a >= 0)
         {
-            Color fade_Transparancy = black_Screen.color;
+            
             
             fade_Transparancy.a -= fade_Speed * Time.deltaTime;
             black_Screen.color = fade_Transparancy;
@@ -40,9 +50,26 @@ public class Scene_Transition : MonoBehaviour
             }
         }
 
-        if (death_Sequence = true && black_Screen.color.a <= 1)
+        if (death_Sequence == true)
         {
-            
+            start_Sequence = false;
+            //print("buh");
+            if (black_Screen.color.a <= 1)
+            {
+                //print("ah");
+
+                fade_Transparancy.a += fade_Speed * Time.deltaTime;
+                black_Screen.color = fade_Transparancy;
+
+                if (black_Screen.color.a >= 1)
+                {
+                    death_Sequence = false;
+                    Scene currentScene = SceneManager.GetActiveScene();
+                    SceneManager.LoadScene(currentScene.buildIndex);
+                }
+            }
         }
-    }
+        
+    }// end Update
+    
 }
